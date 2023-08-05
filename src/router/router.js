af55bf582e45d08a7887c6383e910a210a6092fb
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-08-05 10:02:48 -0400

    No Page Found

--- src/router/router.js
+++ src/router/router.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import ListPage from '../modules/pokemon/pages/ListPage'
 import AboutPage from '../modules/pokemon/pages/AboutPage'
 import PokemonPage from '../modules/pokemon/pages/PokemonPage'
+import NoPageFound from '../modules/shared/components/pages/NoPageFound'
 
 const routes = [
     { path: '/', component: ListPage },
     { path: '/about', component: AboutPage },
     { path: '/id', component: PokemonPage },
+    { path: '/:pathMatch(.*)*', component: NoPageFound },
   ]
 
   const router = createRouter({

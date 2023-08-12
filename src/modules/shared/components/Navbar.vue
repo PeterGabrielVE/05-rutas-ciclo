9ee25e52818db7aa704a705948d5e2f72bbc6369
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-08-12 10:47:00 -0400

    router-link personalizado

--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,11 +1,34 @@
 <template>
     <div>
-        <router-link :to="{ name: 'home' }">Pokemon List</router-link>
+        <!--<router-link :to="{ name: 'home' }">Pokemon List</router-link>
         <router-link :to="{ name: 'pokemon-id', params: { id: 87 } }">Pokemon by id</router-link>
-        <router-link :to="{ name: 'about' }">About</router-link>
+        <router-link :to="{ name: 'about' }">About</router-link>-->
+        <CustomLink v-for="link in links"
+        :key="link.to" 
+        :link="link" 
+        />
     </div>
 </template>
+<script>
+import { defineAsyncComponent } from 'vue'
+export default{
 
+    data(){
+        return{
+            links:[
+               { to: '/home', name:'Pokemons' },
+               { to: '/pokemonid/50', name:'Por ID' },
+               { to: '/about', name:'About' },
+               { to: 'https://google.com', name:'Google' },
+            ]
+        }
+    },
+    components:{
+        CustomLink: defineAsyncComponent( () => import('./CustomLink.vue') )
+    }
+}
+
+</script>
 <style scoped>
     div{
         padding: 0 30px;
